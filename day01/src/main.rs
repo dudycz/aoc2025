60b037ee9ec32198cdc3f66fad8b399f8566ddb9
Rafal Dudycz <rafal.dudycz@gmail.com>
2025-12-12 22:57:23 +0100

    Init

---
+++ day01/src/main.rs
@@ -0,0 +1,27 @@
+// load file.txt line by line
+// start from 50
+// for each line check if is multiplicity of 100
+// handle rotations
+use std::fs::read_to_string;
+
+
+fn main() {
+    let mut input = Vec::new();
+    // lock - init(50)
+    // lock.rotate
+    // lock.get_count
+
+    for line in read_to_string("input.txt").unwrap().lines() {
+        
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    [test]
+    fn test_algo() {
+        //srvlet input
+    }
+}
